7c7b321f5415c1073b1ea271dd6ba489c5d6c3d7
Hamzah-Ahmad <ahmadhamzah500@gmail.com>
2025-07-29 08:10:54 -0500

    setup:connects with db running on docker server

---
+++ src/database/database.module.ts
@@ -0,0 +1,23 @@
+// src/database/database.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {  ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow("DB_HOST"),
+        port: +configService.getOrThrow("DB_PORT"),
+        username: configService.getOrThrow("DB_USERNAME"),
+        password: configService.getOrThrow("DB_PASSWORD"),
+        database: configService.getOrThrow("DB_NAME"),
+        autoLoadEntities: true,
+        synchronize: configService.get<string>('TYPEORM_SYNC', 'false') === 'true', // use false in prod
+      }),
+      inject: [ConfigService]
+    }),
+  ],
+})
+export class DatabaseModule {}
