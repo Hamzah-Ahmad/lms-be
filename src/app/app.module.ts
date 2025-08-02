04e49ba5cb07ade2613ed22d11b1b4ba89ef0bd1
Hamzah-Ahmad <ahmadhamzah500@gmail.com>
2025-08-02 09:40:16 -0500

    setup: folder naming standard update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from 'src/database/database.module';
-import { UsersModule } from 'src/features/users/users.module';
+import { UserModule } from 'src/features/user/user.module';
 import { CoreModule } from 'src/core/core.module';
 
 @Module({
   imports: [
     CoreModule,
     DatabaseModule,
-    UsersModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
