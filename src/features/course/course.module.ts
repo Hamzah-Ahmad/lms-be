ca9f341fc14fd11141b146650181460a2a7053f4
Hamzah-Ahmad <ahmadhamzah500@gmail.com>
2025-08-04 21:38:12 -0500

    registering typeorm modules

--- src/features/course/course.module.ts
+++ src/features/course/course.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Course } from './entities/course.entity';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([Course])]
+})
 export class CourseModule {}
